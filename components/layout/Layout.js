b89fb7bd5524374ecafd2459885caf154054c384
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-12 11:01:06 -0500

    Agregando Fuentes de Googe Fonts y Más

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import { css, Global } from '@emotion/react';
 import React from 'react';
 import Header from './Header';
+import Head from 'next/head';
 
 const Layout = props => {
     return (
@@ -26,6 +27,14 @@ const Layout = props => {
             margin: 0 0 2rem 0;
             line-height: 1.5;
         }
+        h1,h2{
+            font-family:'Roboto Slab',serif;
+            font-weight: 700;
+        }
+        h3{
+            font-family:'PT Sans',sans-serif;
+            font-weight: 700;
+        }
         ul{
             list-style:none;
             margin:0;
@@ -35,6 +44,14 @@ const Layout = props => {
             text-decoration:none;
         }
         `}></Global>
+
+            <Head>
+                <html lang="es"></html>
+                <title>Product reviewer Firebase y Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+                <link rel="stylesheet" href="/statoc/css/app.css" />
+            </Head>
             <Header></Header>
             <main>
                 { props.children }
