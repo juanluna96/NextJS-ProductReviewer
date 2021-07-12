99fb59a1308410f00d0eba82f7f187fd95cae53c
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-12 16:27:16 -0500

    CSS a la Barra

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Buscar from '../ui/Buscar';
-import Navegacion from './Navegacion';
 import Link from 'next/link';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+import Buscar from '../ui/Buscar';
+import Navegacion from './Navegacion';
+import Boton from '../ui/Boton';
 
 const ContenedorHeader = styled.div`
     max-width:1200px;
@@ -26,6 +27,9 @@ const Logo = styled.p`
 `;
 
 const Header = () => {
+
+    const usuario = true;
+
     return (
         <header css={ css`
         border-bottom: 2px solid var(--gris3);
@@ -41,15 +45,39 @@ const Header = () => {
                     {/* Nav aqui */ }
                     <Navegacion></Navegacion>
                 </div>
-                <div className="">
-                    {/* Menu de administracion */ }
-                    <p>Hola: Juan</p>
-                    <button>Cerrar sesión</button>
-                    <Link href="/">Login</Link>
-                    <Link href="/">Crear cuenta</Link>
+                <div css={ css`
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                `}>
+                    {
+                        usuario
+                            ? (
+                                <>
+                                    <p css={ css`
+                                        margin-right: 2rem;
+                                    `}>Hola: Juan</p>
+                                    <Boton bgColor="true">Cerrar sesión</Boton>
+                                </>
+                            )
+                            : (
+                                <>
+                                    <Link passHref href="/">
+                                        <Boton bgColor="true">
+                                            Login
+                                        </Boton>
+                                    </Link>
+                                    <Link passHref href="/">
+                                        <Boton>
+                                            Crear cuenta
+                                        </Boton>
+                                    </Link>
+                                </>
+                            )
+                    }
                 </div>
             </ContenedorHeader>
-        </header>
+        </header >
     )
 }
 
