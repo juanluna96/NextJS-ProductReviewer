e3cd20470eda8614e906d7487307badfa1cc04d7
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-11 22:41:25 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  color:red
+`;
+
+
+const Home = () => {
+  return (
+    <div>
+      <Heading>Inicio</Heading>
+    </div>
+  )
+}
+
+export default Home
