934517b3f8fc28592dae2f9171ec0e0cd4272c14
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-12 10:15:06 -0500

    Agregando CSS Global

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,9 +1,40 @@
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import Header from './Header';
 
 const Layout = props => {
     return (
         <>
+            <Global styles={ css`
+        :root{
+            --gris:#3d3d3d;
+            --gris2:#6F6F6F;
+            --naranja:#DA552F;
+        }
+        html{
+            font-size:62.5%;
+            box-sizing:border-box;
+        }
+        *,*:before,*:after{
+            box-sizing:inherit;
+        }
+        body{
+            font-size:1.6rem;
+            line-height: 1.5;
+        }
+        h1,h2,h3{
+            margin: 0 0 2rem 0;
+            line-height: 1.5;
+        }
+        ul{
+            list-style:none;
+            margin:0;
+            padding:0;
+        }
+        a{
+            text-decoration:none;
+        }
+        `}></Global>
             <Header></Header>
             <main>
                 { props.children }
