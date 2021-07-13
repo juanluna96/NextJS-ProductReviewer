7973a3a65bab409aba4304649c2b22e990486e9e
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-13 13:07:00 -0500

    Creando los archivos de configuración de Firebase

---
+++ firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBat2b7Zk9bdrlv3_64Rz_YkmmreFc-_9k",
+    authDomain: "product-reviewer-b82f1.firebaseapp.com",
+    projectId: "product-reviewer-b82f1",
+    storageBucket: "product-reviewer-b82f1.appspot.com",
+    messagingSenderId: "803688783605",
+    appId: "1:803688783605:web:f8206f06a14b4a46a8efbc"
+};
+
+export default firebaseConfig;
