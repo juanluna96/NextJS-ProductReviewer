367c385f995803070be50f09f98b318c28555949
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-12 21:26:09 -0500

    Creando el Formulario para Crear Cuentas

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -5,7 +5,24 @@ const CrearCuenta = () => {
     return (
         <div>
             <Layout>
-                <h1>Crear cuenta</h1>
+                <>
+                    <h1>Crear cuenta</h1>
+                    <form>
+                        <div>
+                            <label htmlFor="nombre">Nombre</label>
+                            <input type="text" id="nombre" placeholder="Tu nombre" name="nombre" />
+                        </div>
+                        <div>
+                            <label htmlFor="email">Correo electrónico</label>
+                            <input type="email" id="email" placeholder="Tu correo" name="email" />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Correo electrónico</label>
+                            <input type="password" id="password" placeholder="Tu contraseña" name="password" />
+                        </div>
+                        <input type="submit" value="Crear cuenta" />
+                    </form>
+                </>
             </Layout>
         </div>
     )
