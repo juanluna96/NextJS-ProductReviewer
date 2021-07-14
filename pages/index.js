d58f7ca7550b119decb7ccb7e868545a5ebf2299
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-14 13:57:07 -0500

    Consultando la Base de Datos

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Layout from '../components/layout/Layout';
 import styled from '@emotion/styled'
+import { FirebaseContext } from '../firebase';
 
 const Heading = styled.h1`
-  color:red
+  color:red;
 `;
 
-
 const Home = () => {
+  const [productos, setProductos] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db.collection('productos').orderBy('creado', 'desc').onSnapshot(manejarSnapshot)
+    }
+    obtenerProductos();
+  }, []);
+
+  function manejarSnapshot(snapshot) {
+    const productos = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    })
+    );
+
+    setProductos(productos);
+  }
+
   return (
     <div>
       <Layout>
