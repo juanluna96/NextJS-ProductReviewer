f575c53862255777f192c66c492d9b4368520308
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-14 15:56:43 -0500

    Mostrando Información de la base de datos

--- components/layout/DetallesProducto.js
+++ components/layout/DetallesProducto.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import { es } from 'date-fns/locale';
 
 const DetallesProducto = ({ producto }) => {
-    const { id, comentarios, creado, descripcion, empresa, nombre, url, urImagen, votos } = producto;
+    const { id, comentarios, creado, descripcion, empresa, nombre, url, urlImagen, votos } = producto;
 
     return (
         <li>
             <div className="">
                 <div className="">
-
+                    <Image
+                        src={ urlImagen }
+                        alt={ nombre }
+                        width={ 200 }
+                        height={ 200 }
+                    />
                 </div>
                 <div className="">
                     <h1>{ nombre }</h1>
+                    <p>{ descripcion }</p>
+                    <div>
+                        <Image
+                            src="/static/img/comentario.png"
+                            alt={ `comentarios-${id}` }
+                            width={ 200 }
+                            height={ 200 }
+                        />
+                        <p>{ comentarios.length } Comentarios</p>
+                    </div>
+                    <p>Publicado hace: { formatDistanceToNow(new Date(creado), { locale: es }) }</p>
                 </div>
             </div>
-            <div className=""></div>
+            <div>
+                <div>&#9650;</div>
+                <p>{ votos }</p>
+            </div>
         </li>
     )
 }
