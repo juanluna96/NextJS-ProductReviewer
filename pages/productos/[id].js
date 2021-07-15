8e0750c1655c642fb4a3b6ac89a0f8c001c2359a
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-14 22:12:24 -0500

    Validar que un producto exista

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,21 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { useRouter } from 'next/router';
+import { FirebaseContext } from '../../firebase';
+
+import Error404 from '../../components/layout/Error404';
+import Layout from '../components/layout/Layout';
 
 const Producto = () => {
+    // State del componente
+    const [producto, setProducto] = useState('');
+    const [error, setError] = useState(false);
+
     // Routing para obtener el id actual
     const router = useRouter();
     const { id } = router.query;
 
+    const { firebase } = useContext(FirebaseContext);
+
     useEffect(() => {
         if (id) {
-            console.log('Ya hay un id', id)
+            const obtenerProducto = async () => {
+                const productoQuery = await firebase.db.collection('productos').doc(id);
+                const producto = await productoQuery.get();
+                if (producto.exists) {
+                    setProducto(producto.data());
+                } else {
+                    setError(true); s
+                }
+            }
+            obtenerProducto();
         }
     }, [id]);
 
     return (
-        <div>
-            Desde producto n°{ id }
-        </div>
+        <Layout>
+            <>
+                { error && <Error404></Error404> }
+            </>
+        </Layout>
     )
 }
 
