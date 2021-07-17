d00823c36cf4e106a53194089d00175b53a1ff8d
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-16 22:16:57 -0500

    Filtrando los Productos

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/layout/Layout';
 import { useRouter } from 'next/router'
+import DetallesProducto from '../components/layout/DetallesProducto';
+import useProductos from '../hooks/useProductos';
 
 const Buscar = () => {
     const router = useRouter();
     const { q } = router.query;
 
+    // Todos los productos
+    const { productos } = useProductos('creado');
+    const [resultado, setResultado] = useState([]);
+
+    useEffect(() => {
+        const busqueda = q.toLowerCase();
+        const filtro = productos.filter(producto => (producto.nombre.toLowerCase().includes(busqueda) || producto.descripcion.toLowerCase().includes(busqueda)));
+        setResultado(filtro);
+    }, [q, productos]);
+
     return (
         <div>
             <Layout>
-                <h1>Buscar</h1>
+                <div className="listado-productos">
+                    <div className="contened">
+                        <ul className="bg-white">
+                            {
+                                resultado.map(producto => (
+                                    <DetallesProducto key={ producto.id } producto={ producto }></DetallesProducto>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
             </Layout>
         </div>
     )
