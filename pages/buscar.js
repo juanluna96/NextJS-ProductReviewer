0d08edbbb79a44be587e65f6def4240829f40d55
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-16 22:07:23 -0500

    Pasando la búsqueda al componente

--- pages/buscar.js
+++ pages/buscar.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Layout from '../components/layout/Layout';
+import { useRouter } from 'next/router'
 
 const Buscar = () => {
+    const router = useRouter();
+    const { q } = router.query;
+
     return (
         <div>
             <Layout>
