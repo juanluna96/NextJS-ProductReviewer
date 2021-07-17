c41b3cf9fb1196d92eaf4d289f11efd5c5089010
Juan Diego Luna Castañeda <juandluna17@gmail.com>
2021-07-16 21:40:29 -0500

    Primeros pasos con el buscador

--- components/ui/Buscar.js
+++ components/ui/Buscar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 
@@ -29,9 +29,18 @@ const InputSubmit = styled.button`
 
 
 const Buscar = () => {
+    const [busqueda, setBusqueda] = useState('');
+
+    const buscarProducto = (e) => {
+        e.preventDefault();
+        if (busqueda.trim() === "") return;
+
+        // Redireccionar a buscar
+    }
+
     return (
-        <form css={ css`position:relative;` }>
-            <InputText type="text" placeholder="Buscar productos" />
+        <form css={ css`position:relative;` } onSubmit={ buscarProducto }>
+            <InputText type="text" placeholder="Buscar productos" onChange={ e => setBusqueda(e.target.value) } />
             <InputSubmit type="submit"></InputSubmit>
         </form>
     )
